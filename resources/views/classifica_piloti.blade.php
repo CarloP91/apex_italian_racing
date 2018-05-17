bb97a139824b7412bea5d2c548e06ba638895c38
Carlo Pennetta <carlopennetta91@gmail.com>
2018-05-17 13:09:47 +0200

    piloti mod try

--- resources/views/classifica_piloti.blade.php
+++ resources/views/classifica_piloti.blade.php
@@ -184,10 +184,37 @@
     </tr>
 
     <tr>
-      <th scope="row">4</th>
-      <td><img src="/img/auto2018/Mercedes-F12018.jpg" style="width: 200px; height: 50px"></td>
-      <td>100</td>
+      <th scope="row" style="background-color: #cd7f32">4</th>
+<!--Fabiuzz-->
+      <td><!--img scuderia--><img src="{{$drivers[3]->img_url_auto}}" style="width: 200px; height: 50px"></td>
+      <td><!--numero pilota-->20</td>
+      <td><!--nome pilota-->  {{$drivers[3]->name_driver}}</td>
+      <td><!--punti totali--> {{$pt = $drivers[3]->australia + $drivers[3]->bahrain + $drivers[3]->china}} </td></td>
+      
+      <td>{{$drivers[3]->australia}}</td>
+      <td>{{$drivers[3]->bahrain}}</td>
+      <td>{{$drivers[3]->china}}</td>
+      <td>{{$drivers[3]->baku}}</td>
+      <td>{{$drivers[3]->spagna}}</td>
+      <td>{{$drivers[3]->monaco}}</td>
+      <td>{{$drivers[3]->canada}}</td>
+      <td>{{$drivers[3]->francia}}</td>
+      <td>{{$drivers[3]->austria}}</td>
+      <td>{{$drivers[3]->uk}}</td>
+      <td>{{$drivers[3]->germania}}</td>
+      <td>{{$drivers[3]->ungheria}}</td>
+      <td>{{$drivers[3]->belgio}}</td>
+      <td>{{$drivers[3]->italia}}</td>
+      <td>{{$drivers[3]->singapore}}</td>
+      <td>{{$drivers[3]->russia}}</td>
+      <td>{{$drivers[3]->suzuka}}</td>
+      <td>{{$drivers[3]->usa}}</td>
+      <td>{{$drivers[3]->messico}}</td>
+      <td>{{$drivers[3]->brasile}}</td>
+      <td>{{$drivers[3]->eau}}</td>
+      
     </tr>
+
     <tr>
       <th scope="row">5</th>
       <td><img src="/img/auto2018/Mercedes-F12018.jpg" style="width: 200px; height: 50px"></td>
@@ -240,7 +267,7 @@
     </tr>
     <tr>
       <th scope="row">15</th>
-      <td><img src="/img/auto2018/Mercedes-F12018.jpg" style="width: 200px; height: 50px"></td>
+      <td><img src="  " style="width: 200px; height: 50px"></td>
       <td>100</td>
     </tr>
     <tr>
